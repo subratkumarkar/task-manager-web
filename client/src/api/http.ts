118555a790b82f45d5018948b1eebb969c805b82
Subrat Kumar Kar <subratkumar_kar@yahoo.in>
2025-11-21 10:11:46 -0600

    fix url issue

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const api = axios.create({baseURL: 'http://localhost:3001/api'});
+export const api = axios.create({baseURL: '/api'});
 
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
